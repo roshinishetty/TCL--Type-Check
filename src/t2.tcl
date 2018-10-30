2c9e59536b60bb6c5ac4412417381fba876c3e00
roshinishetty <34018704+roshinishetty@users.noreply.github.com>
2018-10-30 13:36:43 +0530

    "test cases"

--- src/t2.tcl
+++ src/t2.tcl
@@ -3,7 +3,7 @@ set in1 "large random";
 set x a;
 set y 28;
 set z 16.2;
-set language(0) TCL
-set nameString "My name is XXX"
-set personA(name) "Dave"
-set copiedValue $x;
+set lan(0) TCL
+set nam "My name is XXX"
+set pA(name) "Dave"
+set copV $x;
--- src/t3.tcl
+++ src/t3.tcl
@@ -1,9 +1,9 @@
-set fruit Califlower
-set magicNumber 25
+set fru Califlower
+set magN 25
 set sum [expr 5+13]
-set float 24.2348
-dict set colours colour1 red colour2 green
-set fruitList [list apple banana orange];
-proc helloWorld {} { puts "Hello, World!" }
+set fame 24.2348
+dict set cols colour1 red colour2 green
+set fList [list apple banana orange];
+proc World {} { puts "Hello, World!" }
 #!/usr/bin/tclsh
-set Z_LABEL "The Capital of New York is: "  
+set LABEL "The Capital of New York is: "  
